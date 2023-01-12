fc4fb6af39e4ca39177ca3195bb7c1db09a71c0a
jordan sankare <jordan.sankare@gmail.com>
2023-01-12 10:56:48 +0100

    display product data on the product page

--- front/js/product.js
+++ front/js/product.js
@@ -6,19 +6,19 @@ async function showArticle() {
 
     console.log(id) // Test
 
-    // Make a request to the API endpoint to fetch the specific article using the id
+    // Make a request to the API endpoint to fetch the specific product using the id
     const response = await fetch(`http://localhost:3000/api/products/${id}`);
     // Parse the response to json
     const data = await response.json();
 
-    // Access the DOM element where you want to display the article
+    // Access the DOM element where you want to display the product
     const itemSection = document.getElementsByClassName('item__img')[0];
     const title = document.getElementById('title');
     const price = document.getElementById('price');
     const description = document.getElementById('description');
     const colors = document.getElementById("colors");
 
-    // Add the product details such as name, description, and image to the articleSection
+    // Add the product details name, description, and image to the itemSection
     itemSection.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
     title.innerHTML = data.name;
     price.innerHTML = data.price;
