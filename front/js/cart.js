51c8bf8f86c73245a1a173954295cf10a88dc6c8
jordan sankare <jordan.sankare@gmail.com>
2023-01-23 12:22:11 +0100

    show all items in cart, with quantity, color, name, price and img. Also shows correct total price and quantity

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,27 +1,70 @@
+function getUniqueIds(cart) {
+  let ids = []
+
+  for (const item of cart) {
+    let found = false
+
+    for (const id of ids) {
+      if (item.id == id) {
+        found = true
+      }
+    }
+
+    if (found === false) {
+      ids.push(item.id)
+    }
+  }
+  return ids
+}
+
+async function getProducts(ids) {
+  let products = []
+
+  for (const id of ids) {
+    const response = await fetch(`http://localhost:3000/api/products/${id}`)
+    const product = await response.json()
+    products.push(product)
+  }
+
+  return products
+}
+
 async function showCart() {
 
-    let cart = localStorage.getItem('cart')
-    let cartContent = await JSON.parse(cart)
+    let cart = JSON.parse(localStorage.getItem('cart'))
+    const ids = getUniqueIds(cart)
+    const products = await getProducts(ids)
     const cartSection = document.getElementById('cart__items')
+    const totalQuantity = document.getElementById('totalQuantity')
+    const totalPrice = document.getElementById('totalPrice')
+    let totalQuantityValue = 0
+    let totalPriceValue = 0
+    let selectedProduct = []
 
-    if (cartContent === null) {
+    if (cart === null) {
         console.log('le panier est vide')
     }
     else {
-        cartSection.innerHTML = `
-            <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+        for (const item of cart) {
+          for (const product of products) {
+            if (product._id == item.id) {
+              selectedProduct = product
+            }
+          }
+          cartSection.innerHTML += `
+            <article class="cart__item" data-id="${item.id}" data-color="${item.color}">
                 <div class="cart__item__img">
-                  <img src="../images/product01.jpg" alt="Photographie d'un canapé">
+                  <img src="${selectedProduct.imageUrl}" alt="${selectedProduct.altTxt}">
                 </div>
                 <div class="cart__item__content">
                   <div class="cart__item__content__description">
-                    <h2>Nom du produit</h2>
-                    <p>Vert</p>
-                    <p>42,00 €</p>
+                    <h2>${selectedProduct.name}</h2>
+                    <p>${item.color}</p>
+                    <p>${selectedProduct.price} €</p>
                   </div>
                   <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
-                      <p>Qté : </p>
+                      <p>Qté : ${item.quantity}</p>
                       <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
                     </div>
                     <div class="cart__item__content__settings__delete">
@@ -32,6 +75,12 @@ async function showCart() {
               </article>
         `
         console.log('le panier n\'est pas vide')
+        totalQuantityValue += +item.quantity 
+        totalPriceValue += item.quantity * selectedProduct.price
+        }
+        
+        totalPrice.innerHTML = `${totalPriceValue} €`
+        totalQuantity.innerHTML = totalQuantityValue
     }
 }
 showCart()
