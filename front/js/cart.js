fc217ecc246ce1312da7d3017079e52235871c52
Brubru <jordan.sankare@gmail.com>
2023-02-08 14:16:15 +0100

    form now works with regex as intended

--- front/js/cart.js
+++ front/js/cart.js
@@ -218,7 +218,8 @@ const cityRegex = /^[a-zA-Z\s]+$/
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 
 const finishOrder = () => {
-  button.addEventListener("click", function () {
+  button.addEventListener("click", function (event) {
+    event.preventDefault()
     let error = false;
 
     if (!nameRegex.test(inputs.firstName.value)) {
